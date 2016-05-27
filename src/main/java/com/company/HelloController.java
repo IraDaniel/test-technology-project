e3691c6c710a2ac2a3f5530ab7c810a4e69be1a9
Irina_Daniel <Irina_Daniel@epam.com>
2016-05-27 14:49:21 +0300

    Simple project with spring

---
+++ src/main/java/com/company/HelloController.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Irina_Daniel on 5/27/2016.
+ */
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+
+        model.addAttribute("message", "Spring 3 MVC - Hello World");
+        return "welcome";
+
+    }
+}
